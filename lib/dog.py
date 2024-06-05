430e5055d8d896f85df8c5ca994ab67d5e282e4f
Alessandro Ledesma <sandroledesma@gmail.com>
2024-06-05 13:21:13 -0400

    update files

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name="Mutt", breed="Mutt"):
+        self.name = name
+        self.breed = breed
+    
+    def bark(self): 
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog("Fido")
+
+snoopy = Dog("Snoopy")
